e68d3eaa841cf4cdf5049cb071ab57a6cdfabd9f
Onno Haldar <onno@zgw.nu>
2019-02-10 19:45:23 +0100

    refactor

---
+++ src/ng-joint-schematics/shape/index.ts
@@ -0,0 +1,59 @@
+import { strings } from '@angular-devkit/core';
+import {
+  Rule,
+  SchematicContext,
+  SchematicsException,
+  Tree,
+  apply,
+  applyTemplates,
+  branchAndMerge,
+  chain,
+  filter,
+  mergeWith,
+  move,
+  noop,
+  url,
+} from '@angular-devkit/schematics';
+import { applyLintFix } from '@schematics/angular/utility/lint-fix';
+
+import { parseName } from '@schematics/angular/utility/parse-name';
+import { buildDefaultPath, getProject } from '@schematics/angular/utility/project';
+import { Schema as ClassOptions } from '@schematics/angular/class/schema';
+
+export function ngJointShapeSchematics(options: ClassOptions): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    if (!options.project) {
+      throw new SchematicsException('Option (project) is required.');
+    }
+
+    const project = getProject(host, options.project);
+
+    if (options.path === undefined) {
+      options.path = buildDefaultPath(project);
+    }
+
+    options.type = !!options.type ? `.${options.type}` : '';
+
+    const parsedPath = parseName(options.path, options.name);
+    options.name = parsedPath.name;
+    options.path = parsedPath.path;
+
+    // todo remove these when we remove the deprecations
+    options.skipTests = options.skipTests || !options.spec;
+
+    const templateSource = apply(url('./files'), [
+      options.skipTests ? filter(path => !path.endsWith('.spec.ts.template')) : noop(),
+      applyTemplates({
+        ...strings,
+        ...options,
+      }),
+      move(parsedPath.path),
+    ]);
+
+    const rule = chain([
+      branchAndMerge(mergeWith(templateSource)),
+      options.lintFix ? applyLintFix(options.path) : noop(),
+    ]);
+    return rule(host, context);
+  };
+}

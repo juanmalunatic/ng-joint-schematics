59e9daf606f9d39df3a7809aef2a0c335f222ed6
Onno Haldar <onno@zgw.nu>
2019-02-11 12:01:00 +0100

    refactor

--- src/build-schemas.ts
+++ src/build-schemas.ts
@@ -1,13 +1,16 @@
 // Build schema.ts files from schema.json files with typescript interfaces
 import { compileFromFile } from 'json-schema-to-typescript';
-import { writeFileSync } from 'fs';
+import { writeFileSync, readFileSync } from 'fs';
 import { join } from 'path';
+import * as Collection from '../node_modules/@angular-devkit/schematics/collection-schema';
 
-const collection = 'ng-joint-schematics';
-const schematics = ['shape'];
+const schematics = 'ng-joint-schematics';
+const jsonCollectionFile = join(__dirname, 'collection.json');
+const data = readFileSync(jsonCollectionFile, 'utf-8');
+const collection: Collection.Schema = JSON.parse(data);
 
-for (const schematic of schematics) {
-        const jsonSchemaPath = join(__dirname, collection, schematic);
+for (const schematic in collection.schematics) {
+        const jsonSchemaPath = join(__dirname, schematics, schematic);
         const jsonSchemaFile = join(jsonSchemaPath, 'schema.json');
         const srcSchemaFile = join(jsonSchemaPath, 'schema.d.ts');
         compileFromFile(jsonSchemaFile)

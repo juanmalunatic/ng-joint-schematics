11430f07237116459fd057e16093088a7ec5f721
Onno Haldar <onno@zgw.nu>
2019-02-26 00:04:40 +0100

    refactor

--- src/ng-joint-schematics/build/index.ts
+++ src/ng-joint-schematics/build/index.ts
@@ -1,13 +1,24 @@
-// Angular Imports
+// To-Do
+// Build commnand line exec: https://stackoverflow.com/questions/20643470/execute-a-command-line-binary-with-node-js
 import { resolve } from 'path';
-// import { strings } from '@angular-devkit/core';
+// Angular Imports
+import { strings } from '@angular-devkit/core';
 import {
     Rule,
     SchematicContext,
     chain,
     Tree,
-    SchematicsException
+    SchematicsException,
+    apply,
+    url,
+    filter,
+    noop,
+    applyTemplates,
+    move,
+    mergeWith,
+    MergeStrategy
 } from '@angular-devkit/schematics';
+import { applyLintFix } from '@schematics/angular/utility/lint-fix';
 import { buildDefaultPath, getProject } from '@schematics/angular/utility/project';
 import { parseName } from '@schematics/angular/utility/parse-name';
 
@@ -41,13 +52,36 @@ export function ngJointBuildSchematics(options: Schema): Rule {
         const location = parseName(rootPath, options.name);
         options.path = location.path;
 
-        console.log(options);
-
         const schematicsData = getSchematicsData(options);
+        const shapes = schematicsData.shapes;
+        options.ngCliBuildStatements = '// start: ' + options.name + '\n\n';
 
-        console.log(schematicsData);
+        for (const shapeType in shapes) {
+            const elements = shapes[shapeType].elements;
 
-        const rule = chain([]);
+            for (const element in elements) {
+                options.ngCliBuildStatements += 'ng g ng-joint-schematics:element ' + shapeType + ' ' + element + ' --project=ng-joint\n';
+            }
+
+            const links = shapes[shapeType].links;
+            for (const link in links) {
+                options.ngCliBuildStatements += 'ng g ng-joint-schematics:link ' + shapeType + ' ' + link + ' --project=ng-joint\n';
+            }
+        }
+
+        const templateBuildSource = apply(url('./files'), [
+            options.skipTests ? filter(path => !path.endsWith('.spec.ts.template')) : noop(),
+            applyTemplates({
+              ...strings,
+              ...options,
+            }),
+            move(options.path)
+          ]);
+
+        const rule = chain([
+            mergeWith(templateBuildSource, MergeStrategy.Default),
+            options.lintFix ? applyLintFix(options.path) : noop(),
+        ]);
         return rule(host, context);
     };
 }

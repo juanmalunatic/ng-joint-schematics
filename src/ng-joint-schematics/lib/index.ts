0ea4d0d8b8e400827da8347a92a278c1b5c0495c
Onno Haldar <onno@zgw.nu>
2019-02-25 19:29:25 +0100

    refactor

--- src/ng-joint-schematics/lib/index.ts
+++ src/ng-joint-schematics/lib/index.ts
@@ -1,19 +1,33 @@
 import {
     Rule,
-    chain
+    Tree,
+    SchematicContext
 } from '@angular-devkit/schematics';
-// import { getSchematicsData } from '../../ng-joint-schematics-data';
-// import {
-//    resolvePath
-//} from '../shape/shape-utils';
+import { getSchematicsData } from '../../ng-joint-schematics-data';
+import { resolveOptionPaths } from '../shape/shape-utils';
+import { Schema as ShapeSchema } from '../../schemas/ng-joint-shape-schema';
 import { Schema } from '../../schemas/ng-joint-lib-schema';
+
+function buildShapeOptions(options: Schema): ShapeSchema {
+    return {
+        shapeType: '',
+        project: options.project,
+        name: ''
+    }
+}
+
 
 /**
  * Joint Js Element Generation Schematics
  * @param options
  */
 export function ngJointLibSchematics(options: Schema): Rule {
-    console.log('ngJointLibSchematics(options) = ', options);
-
-    return chain([]);
+    return (host: Tree, context: SchematicContext) => {
+        const 
+        resolveOptionPaths(host, options);
+        options.implementation = 'element';
+        const rule = ngJointShapeSchematics(options);
+        return rule(host, context);
+    };
 }
+    

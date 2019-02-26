eec89045e94bd55ed373c5c82e6ed4c3320f8ec5
Onno Haldar <onno@zgw.nu>
2019-02-26 13:17:08 +0100

    refactor

--- src/ng-joint-schematics/build/build-utils.ts
+++ src/ng-joint-schematics/build/build-utils.ts
@@ -1,44 +1,25 @@
 import * as cli from '@angular/cli';
-// import { Observable } from 'rxjs';
-
-/*
-function ngCli(cliArgs: string[]): Observable<number> {
-    return new Observable<number>(observer => {
-        cli.default({ cliArgs: cliArgs })
-            .then((response) => {
-                observer.next(response);
-                observer.complete();
-            })
-            .catch((error) => {
-                observer.error(error);
-            });
-        });
-}
-*/
 
 // testing
 console.log('start test syncNgCli');
 let cmdChain = [['help'], ['help']];
-chainNgCmds(cmdChain);
+chainNgCliCmds(cmdChain);
 
-
-function chainNgCmds(cmdChain: string[][], chainIndex?: number) {
+/**
+ * Chains Commands
+ * @param cmdChain [['cmd1arg1', 'cmd1arg2'], ..., ['cmd(n)arg1',...,  'cmd(n)arg(m)'] ] 
+ * @param chainIndex?
+ */
+export function chainNgCliCmds(cmdChain: string[][], chainIndex?: number) {
     let index = 0;
 
     if (chainIndex) {
         if (cmdChain.length === chainIndex) { return; }
         index = chainIndex;
     }
-/*
-    ngCli(cmdChain[index]).subscribe(
-        response => console.log(response),
-        error => console.log(error),
-        () => chainNgCmds(cmdChain, index + 1)
-    );
-*/
 
     cli.default({ cliArgs: cmdChain[index] })
-        .then(() => chainNgCmds(cmdChain, index + 1))
+        .then(() => chainNgCliCmds(cmdChain, index + 1))
         .catch((error) => console.log('chainNgCmds ERROR = ', error));
 
 }

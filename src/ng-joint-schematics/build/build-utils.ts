19c43dcfd2cb8f016d95cb6ffba4192a87e53c13
Onno Haldar <onno@zgw.nu>
2019-02-26 13:12:42 +0100

    refactor

--- src/ng-joint-schematics/build/build-utils.ts
+++ src/ng-joint-schematics/build/build-utils.ts
@@ -1,6 +1,7 @@
 import * as cli from '@angular/cli';
-import { Observable } from 'rxjs';
+// import { Observable } from 'rxjs';
 
+/*
 function ngCli(cliArgs: string[]): Observable<number> {
     return new Observable<number>(observer => {
         cli.default({ cliArgs: cliArgs })
@@ -13,21 +14,32 @@ function ngCli(cliArgs: string[]): Observable<number> {
             });
         });
 }
+*/
 
 // testing
 console.log('start test syncNgCli');
-let cmdChain = [['help'],['help']];
+let cmdChain = [['help'], ['help']];
 chainNgCmds(cmdChain);
 
 
 function chainNgCmds(cmdChain: string[][], chainIndex?: number) {
     let index = 0;
-    if (chainIndex) { index = chainIndex; }
 
+    if (chainIndex) {
+        if (cmdChain.length === chainIndex) { return; }
+        index = chainIndex;
+    }
+/*
     ngCli(cmdChain[index]).subscribe(
         response => console.log(response),
         error => console.log(error),
         () => chainNgCmds(cmdChain, index + 1)
-    );    
+    );
+*/
+
+    cli.default({ cliArgs: cmdChain[index] })
+        .then(() => chainNgCmds(cmdChain, index + 1))
+        .catch((error) => console.log('chainNgCmds ERROR = ', error));
+
 }
 

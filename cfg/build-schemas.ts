c8aa61815ca73013fb0f6f6e7dd7ec602d40773b
Onno Haldar <onno@zgw.nu>
2019-02-28 11:15:58 +0100

    refactor

--- cfg/build-schemas.ts
+++ cfg/build-schemas.ts
@@ -1,11 +1,12 @@
 // Build schema.ts files from schema.json files with typescript interfaces
 import { compileFromFile } from 'json-schema-to-typescript';
 import { writeFileSync, readFileSync } from 'fs';
-import { join } from 'path';
+import { join, resolve } from 'path';
 import * as Collection from '@angular-devkit/schematics/collection-schema';
 
 // read schematics collection
-const jsonCollectionFile = join(__dirname, 'collection.json');
+const srcPath = resolve(__dirname, '..', 'src');
+const jsonCollectionFile = join(srcPath, 'collection.json');
 const data = readFileSync(jsonCollectionFile, 'utf-8');
 const collection: Collection.Schema = JSON.parse(data);
 
@@ -13,12 +14,13 @@ for (const schematic in collection.schematics) {
         // loop through defined schematics and create typescript definitions files
         if (collection.schematics[schematic].schema) {
                 // schema is defined
-                const jsonSchemaFile = join(__dirname, collection.schematics[schematic].schema || '');
+                const jsonSchemaFile = join(srcPath, collection.schematics[schematic].schema || '');
                 const tsDefSchemaFile = jsonSchemaFile.split('.json')[0] + '.d.ts';
                 // create new typescript definition file with interface from json-schema
                 compileFromFile(jsonSchemaFile)
                         .then(tsSchemaInterface => {
                                 writeFileSync(tsDefSchemaFile, tsSchemaInterface);
+                                console.log(jsonSchemaFile + ' compiled to => ' + tsDefSchemaFile);
                         });
         }
 }
